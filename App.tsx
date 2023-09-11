70f3f034284ecef52899240fef6751ec63595f54
Danilo Henrique <danilohssantana@gmail.com>
2023-09-11 19:31:12 -0300

    creating tag for notifications

--- App.tsx
+++ App.tsx
@@ -11,13 +11,18 @@ import { Routes } from "./src/routes";
 
 import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
+import { tagUserEmailCreate } from "./src/notifications/notificationsTags";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 
 OneSignal.setAppId("01a73b9c-0863-4c31-a449-fd223ebbf955");
 
+OneSignal.setEmail("danilo@email.com");
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("danilo@email.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
